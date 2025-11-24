345cf6f27aa38de82bc939b651f3934841ff76a3
Informatika SMA Trinitas <197331326+InformatikaTrinitas@users.noreply.github.com>
2025-11-24 11:51:09 +0700

    Add basic C++ program to check learning status

---
+++ kode_bool.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool sedangBelajar = true; // true/false
+    if (sedangBelajar) {
+        cout << "Saya sedang belajar C++" << endl;
+    } else {
+        cout << "Saya sedang tidak belajar" << endl;
+    }
+    return 0;
+}
